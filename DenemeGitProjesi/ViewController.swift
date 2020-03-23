54e1ab28136aea5bc55c195f4173d5b657d4b44e
r <r>
2020-03-23 03:27:02 +0300

    gizli bilgilerle dolu uygulamam

---
+++ DenemeGitProjesi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DenemeGitProjesi
+//
+//  Created by ramazan çelikkaya on 23.03.2020.
+//  Copyright © 2020 ramazan çelikkaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
