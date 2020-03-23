ef25ff21fce0ef0ba225a46717ab819c51f60a5e
r <r>
2020-03-23 03:51:25 +0300

    şimdi de celli hallettik

---
+++ DenemeGitProjesi/yeniTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  yeniTableViewCell.swift
+//  DenemeGitProjesi
+//
+//  Created by ramazan çelikkaya on 23.03.2020.
+//  Copyright © 2020 ramazan çelikkaya. All rights reserved.
+//
+
+import UIKit
+
+class yeniTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
