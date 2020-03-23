9d1a5c3ed74c16e63103d0ca83b4a693fb6e0827
r <r>
2020-03-23 03:39:44 +0300

    şimdi bir tane view kontroller ekledim

---
+++ DenemeGitProjesi/EkleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EkleViewController.swift
+//  DenemeGitProjesi
+//
+//  Created by ramazan çelikkaya on 23.03.2020.
+//  Copyright © 2020 ramazan çelikkaya. All rights reserved.
+//
+
+import UIKit
+
+class EkleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
